69a9c625005a3203c62b2393379354fc452833c9
Luis Alberto Mendoza <luis.mendoza@uthh.edu.mx>
2025-02-20 08:40:59 -0600

    consulta a fecth

---
+++ app/screens/Tienda.tsx
@@ -0,0 +1,72 @@
+import { ActivityIndicator, Button, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import Boton from '../Components/Boton'
+
+
+const Tienda = () => {
+
+    
+    type prod={
+        id:number,
+        title:string,
+        price:number,
+        description:string,
+        category:string,
+        image:string,
+        rating:{
+            rate:number,
+            count:number
+        }
+    }
+
+    const [producto,setProducto]=useState<prod>();
+    const [loading,setLoading]=useState<boolean>(true);
+    
+    const loadData= async()=>{
+        try{
+            const respuesta=await fetch('https://fakestoreapi.com/products/1');
+            if(!respuesta.ok){
+                console.log('error1');
+                throw new Error('Ocurrio el error : ${respuesta.status}');
+            }
+            const datos= await respuesta.json();
+            console.log(datos);
+            setProducto(datos);
+
+        }catch(e){
+            console.log('Error : '+e)
+        }finally{
+            setLoading(false);
+        }
+    }
+
+    const screenload=()=>{
+        return(
+            <View>
+                <Text>Tienda</Text>
+                <Text>{producto.title}</Text>
+            </View>
+        )
+    
+    }
+
+    const screenUnLoad=()=>{
+        return(
+        <View>
+            <Boton titulo='Carga datos' onPress={loadData}/>
+            <Text>Cargando datos</Text>
+            <ActivityIndicator/>
+        </View>
+        )
+        
+    }
+  return (
+    <View>
+        {loading?screenUnLoad():screenload()}
+    </View>
+  )
+}
+
+export default Tienda
+
+const styles = StyleSheet.create({})
--- index.ts
+++ index.ts
@@ -2,8 +2,9 @@ import { registerRootComponent } from 'expo';
 
 import App from './App';
 import Login from './app/screens/Login';
+import Tienda from './app/screens/Tienda';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(Tienda);
